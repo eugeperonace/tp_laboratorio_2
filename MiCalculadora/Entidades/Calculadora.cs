464c1cb1f5dc38b24e9f380df5a695cd37940547
unknown <eugeperonace@gmail.com>
2021-04-15 23:58:20 -0300

    Trabajo Práctico 1

---
+++ MiCalculadora/Entidades/Calculadora.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public static class Calculadora
+    {
+        public static double Operar(Numero num1, Numero num2, string operador)
+        {
+
+        }
+
+        private static string ValidarOperador(char operador)
+        {
+            string retorno;
+
+            return retorno;
+
+        }
+    }
+}
---
+++ MiCalculadora/Entidades/Numero.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Numero
+    {
+        private double numero;
+
+        Numero()
+        {
+            this.numero = 0;
+        }
+
+        Numero(double numero)
+        {
+            this.numero = numero;
+        }
+
+        Numero(string numero)
+        {
+
+        }
+
+        public double ValidarNumero(string strNumero)
+        {
+            double retorno;
+            retorno = 0;
+
+            return retorno;
+        }
+
+        public string SetNumero
+        {
+            //previa validación con método ValidarNumero
+            set { }
+        }
+
+        public string BinarioDecimal(string binario)
+        {
+
+        }
+
+        public string DecimalBinario(double decimal) 
+        { 
+        }
+
+        public string DecimalBinario(string decimal)
+        {
+
+        }
+
+        private bool EsBinario(string binario)
+        {
+
+        }
+    }
+}
