29e33a09d49c381141b19c291e6dac8a05b61483
unknown <eugeperonace@gmail.com>
2021-04-18 12:11:39 -0300

    Entidad Calculadora

--- MiCalculadora/Entidades/Calculadora.cs
+++ MiCalculadora/Entidades/Calculadora.cs
@@ -10,15 +10,43 @@ namespace Entidades
     {
         public static double Operar(Numero num1, Numero num2, string operador)
         {
+            double resultado = 0;
 
+            char operadorChar = Convert.ToChar(operador);
+            string operadorValidado = ValidarOperador(operadorChar);
+
+                switch (operadorValidado)
+                {
+                    case "+":
+                        resultado = num1 + num2;
+                        break;
+                    case "-":
+                        resultado = num1 - num2;
+                        break;
+                    case "*":
+                        resultado = num1 * num2;
+                        break;
+                    case "/":
+                        resultado = num1 / num2;
+                        break;
+                    default:
+                        break;
+                }
+
+            return resultado;
         }
 
         private static string ValidarOperador(char operador)
         {
             string retorno;
 
-            return retorno;
+            if (operador != '+' || operador != '-' || operador != '*' || operador != '/')
+                retorno = "+";
 
+            else
+                retorno = Convert.ToString(operador);
+
+            return retorno;
         }
     }
 }
